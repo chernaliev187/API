5f672b02906a3eb0a727500e8276118491cff5ee
chernaliev187 <20215@uktc-bg.com>
2024-06-12 16:18:42 +0300

    Update project.model.js

---
+++ proekt api/models/project.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const projectSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Project = mongoose.model('Project', projectSchema);
+
+module.exports = Project;
